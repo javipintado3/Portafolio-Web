09cf402fea00c2ea4231f68a548f517021d2aaa4
Javier Pintado Navarro <javiertoncns03@gmail.com>
2024-08-13 18:28:19 +0200

    Creación del home y layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { SobreMiComponent } from './Components/sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './Components/proyectos/proyectos.component';
+import { ContactosComponent } from './Components/contactos/contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path:'sobremi', component:SobreMiComponent},
+  {path:'proyectos',component:ProyectosComponent},
+  {path:'contacto',component:ContactosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProyectosComponent } from './Components/proyectos/proyectos.component';
+import { SobreMiComponent } from './Components/sobre-mi/sobre-mi.component';
+import { ContactosComponent } from './Components/contactos/contactos.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProyectosComponent,
+    SobreMiComponent,
+    ContactosComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
