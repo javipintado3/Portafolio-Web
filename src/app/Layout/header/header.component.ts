eb5afc61be273075f90c6e2b6f07737d30d4ffee
Javier Pintado Navarro <javiertoncns03@gmail.com>
2024-09-04 12:46:36 +0200

    Mejora visual en móviles

--- src/app/Layout/header/header.component.ts
+++ src/app/Layout/header/header.component.ts
@@ -1,18 +1,25 @@
-import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  menuActive: boolean = false;  // Controla el estado del menú
+
   constructor(private router: Router, private viewportScroller: ViewportScroller) {}
 
   scrollToSection(section: string): void {
     this.router.navigate(['/home'], { fragment: section }).then(() => {
       this.viewportScroller.scrollToAnchor(section);
     });
+    this.menuActive = false; // Cierra el menú al hacer clic en un enlace
+  }
+
+  toggleMenu(): void {
+    this.menuActive = !this.menuActive; // Alterna el estado del menú
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
-import { ProyectosComponent } from './Components/proyectos/proyectos.component';
-import { SobreMiComponent } from './Components/sobre-mi/sobre-mi.component';
 import { ContactosComponent } from './Components/contactos/contactos.component';
 import { HeaderComponent } from './Layout/header/header.component';
 import { FooterComponent } from './Layout/footer/footer.component';
+
+// Importaciones de Font Awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Paquete de íconos "solid"
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProyectosComponent,
-    SobreMiComponent,
     ContactosComponent,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule  // Importa Font Awesome Module aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  // Configurar los iconos en el constructor
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);  // Agrega todos los íconos del paquete 'solid'
+  }
+}
